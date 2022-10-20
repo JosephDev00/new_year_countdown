906ecc5266ae958a1d6f5cd9ff55cad1a9c65041
macbookpro <josephnew584@gmail.com>
2022-10-20 17:16:43 +0530

    .

---
+++ main.js
@@ -0,0 +1,24 @@
+const day=document.querySelector("#day");
+const hours=document.querySelector("#hours");
+const minutes=document.querySelector("#minutes");
+const seconds=document.querySelector("#seconds");
+
+
+function UpdateYear() {
+    const currentyear = new Date().getFullYear();
+    const newYear = new Date(`January 1 ${currentyear + 1} 00:00:00`);
+    const currentDate = new Date();
+    const diff = newYear - currentDate;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor((diff / 1000 / 60 / 60) % 24);
+    const m = Math.floor((diff / 1000 / 60) % 60);
+    const s = Math.floor((diff / 1000) % 60);
+
+    day.innerHTML=d;
+    hours.innerHTML=h;
+    minutes.innerHTML=m;
+    seconds.innerHTML=s;
+
+}
+
+setInterval(UpdateYear,1000);
